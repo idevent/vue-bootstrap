6e970cb97e2d025d981acb5cb59bc2aa56009983
张裴裴 <zhangpp@idevent.cn>
2016-11-10 16:57:40 +0800

    first run success

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 import Vue from 'vue'
+import Router from 'vue-router'
+
+import routes from './routes'
 import App from './App'
 
+Vue.use(Router)
+const router = new Router({
+	  routes
+})
+
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
+const app = new Vue({
+	el: '#app',
+  router: router,
   template: '<App/>',
-  components: { App }
+  components: {
+      App
+  }
 })
---
+++ src/routes.js
@@ -0,0 +1,4 @@
+export default [{
+	  path: '/',
+	  component: require('components/Hello.vue')
+}]
